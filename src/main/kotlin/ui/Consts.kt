7cbd2eaf8f0cd2ee67f1b107569b291e1b41a9fd
Rasmus Moorats <xx@nns.ee>
2024-07-03 18:05:37 +0300

    update editor hint

--- src/main/kotlin/ui/Consts.kt
+++ src/main/kotlin/ui/Consts.kt
@@ -3,12 +3,14 @@ package burp.ui
 const val TRANSFORMER_EDITOR_HINT = """// Transformer function JavaScript code
 
 // value: string - the value to transform
+// values: object - contains key-value pairs of all values
 // Lib: object - the library object (contains lodash, etc.)
+
 // returns: string - the transformed value (last line is returned implicitly)
 
 // Example 1:
 // value.toUpperCase()
 
 // Example 2:
-// Lib._.camelCase(value)
+// Lib._.camelCase(values.refreshToken)
 """
